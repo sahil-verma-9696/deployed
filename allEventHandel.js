eac8d0ea9c170d8f69a9c691f11cc0e2c1d93b8c
Sahil verma <129593012+sahil-verma-9696@users.noreply.github.com>
2023-07-03 02:55:41 +0530

    introducing oops in js
    add a clear all button which relode the page only

--- allEventHandel.js
+++ allEventHandel.js
@@ -1,59 +1,59 @@
-let flag = "none";
-text[1].style.display = "none";
-text[2].style.display = "none";
-text[3].style.display = "none";
-operator.style.display = "none"
-secondryConstant.style.display = "none";
-primaryConstant.style.display = "none";
-secondryRowObject.style.display = "none";
-secondryColObject.style.display = "none";
-primaryMat.style.display = "none"
-secondryMat.style.display = "none"
+let flag = "Addition";
+calc.element.text[1].style.display = "none";
+calc.element.text[2].style.display = "none";
+calc.element.text[3].style.display = "none";
+calc.element.operator.style.display = "none"
+calc.element.secondryConstant.style.display = "none";
+calc.element.primaryConstant.style.display = "none";
+calc.element.secondryRowObject.style.display = "none";
+calc.element.secondryColObject.style.display = "none";
+calc.element.primaryMat.style.display = "none"
+calc.element.secondryMat.style.display = "none"
 
 // =========================== Modes selection =================== 
 
-modes.addEventListener("click", (event) => {
-    if (modes.value == "Addition") {
-        operator.value = "+";
-        text[0].style.display = "inline";
-        text[1].style.display = "none";
-        text[2].style.display = "none";
-        text[3].style.display = "none";
-        secondryRowObject.style.display = "none";
-        secondryColObject.style.display = "none";
+calc.element.modes.addEventListener("click", (event) => {
+    if (calc.element.modes.value == "Addition") {
+        calc.element.operator.value = "+";
+        calc.element.text[0].style.display = "inline";
+        calc.element.text[1].style.display = "none";
+        calc.element.text[2].style.display = "none";
+        calc.element.text[3].style.display = "none";
+        calc.element.secondryRowObject.style.display = "none";
+        calc.element.secondryColObject.style.display = "none";
         flag = "Addition";
     }
-    if (modes.value == "Subtraction") {
-        operator.value = "-";
-        text[0].style.display = "inline";
-        text[1].style.display = "none";
-        text[2].style.display = "none";
-        text[3].style.display = "none";
-        secondryRowObject.style.display = "none";
-        secondryColObject.style.display = "none";
+    if (calc.element.modes.value == "Subtraction") {
+        calc.element.operator.value = "-";
+        calc.element.text[0].style.display = "inline";
+        calc.element.text[1].style.display = "none";
+        calc.element.text[2].style.display = "none";
+        calc.element.text[3].style.display = "none";
+        calc.element.secondryRowObject.style.display = "none";
+        calc.element.secondryColObject.style.display = "none";
         flag = "Subtraction";
     }
-    if (modes.value == "Multiplication") {
-        operator.value = "x";
-        text[0].style.display = "none";
-        text[1].style.display = "inline";
-        text[2].style.display = "inline";
-        text[3].style.display = "inline";
-        secondryRowObject.style.display = "inline";
-        secondryColObject.style.display = "inline";
+    if (calc.element.modes.value == "Multiplication") {
+        calc.element.operator.value = "x";
+        calc.element.text[0].style.display = "none";
+        calc.element.text[1].style.display = "inline";
+        calc.element.text[2].style.display = "inline";
+        calc.element.text[3].style.display = "inline";
+        calc.element.secondryRowObject.style.display = "inline";
+        calc.element.secondryColObject.style.display = "inline";
         flag = "Multiplication";
 
     }
-    if (modes.value == "Determinant") {
-        text[0].style.display = "inline";
-        text[1].style.display = "none";
-        text[2].style.display = "none";
-        text[3].style.display = "none";
-        operator.style.display = "none"
-        secondryConstant.style.display = "none";
-        primaryConstant.style.display = "none";
-        secondryRowObject.style.display = "none";
-        secondryColObject.style.display = "none";
+    if (calc.element.modes.value == "Determinant") {
+        calc.element.text[0].style.display = "inline";
+        calc.element.text[1].style.display = "none";
+        calc.element.text[2].style.display = "none";
+        calc.element.text[3].style.display = "none";
+        calc.element.operator.style.display = "none"
+        calc.element.secondryConstant.style.display = "none";
+        calc.element.primaryConstant.style.display = "none";
+        calc.element.secondryRowObject.style.display = "none";
+        calc.element.secondryColObject.style.display = "none";
         flag = "Determinant";
     }
 
@@ -61,54 +61,59 @@ modes.addEventListener("click", (event) => {
 
 // =========================== order complete =================== 
 function primRowMove() {// run when keyup on p-row ip
-    if (primaryRowObject.value.length == 1) {
-        primaryColObject.focus();
+    if (calc.element.primaryRowObject.value.length == 1) {
+        calc.element.primaryColObject.focus();
     }
 }
 function primColMove() {// run when keyup on p-col ip
-    if (modes.value == "Multiplication") {
-        secondryColObject.focus();
-        secondryRowObject.value = primaryColObject.value;
+    
+    row = parseInt(calc.element.primaryRowObject.value);
+    col = parseInt(calc.element.primaryColObject.value);
+
+    if (calc.element.modes.value == "Multiplication") {
+        calc.element.secondryColObject.focus();
+        calc.element.secondryRowObject.value = calc.element.primaryColObject.value;
     } else {
-        inputMat.focus();
+        calc.element.inputMat.focus();
     }
 }
 
 function secondRowMove() {// run when keyup on s-row ip
-    if (primaryRowObject.value.length == 1) {
-        secondryColObject.focus();
+    if (calc.element.primaryRowObject.value.length == 1) {
+        calc.element.secondryColObject.focus();
     }
 }
 function secondColMove() {// run when keyup on s-col ip
-    if (primaryRowObject.value.length == 1) {
-        inputMat.focus();
+    sCol = parseInt(calc.element.secondryColObject.value);
+    if (calc.element.primaryRowObject.value.length == 1) {
+        calc.element.inputMat.focus();
     }
 }
 
-primaryRowObject.addEventListener("keyup", (event) => {
+calc.element.primaryRowObject.addEventListener("keyup", (event) => {
     if (event.key == "Enter") {
-        primaryColObject.focus();
+        calc.element.primaryColObject.focus();
     }
 });
 
-primaryColObject.addEventListener("keyup", (event) => {
+calc.element.primaryColObject.addEventListener("keyup", (event) => {
     if (event.key == "Enter") {
-        inputMat.focus();
+        calc.element.inputMat.focus();
     }
 
-    if (primaryRowObject.value.length > 1 && primaryColObject.value.length > 1) {
+    if (calc.element.primaryRowObject.value.length > 1 && calc.element.primaryColObject.value.length > 1) {
         alert("Enter value is high")
     }
 
 });
 inputMat.addEventListener("click", (event) => {
-    text[0].style.display = "inline";
-    text[1].style.display = "none";
-    text[2].style.display = "none";
-    text[3].style.display = "none";
-    operator.style.display = "inline"
-    secondryConstant.style.display = "inline";
-    primaryConstant.style.display = "inline";
-    primaryMat.style.display = "inline";
-    secondryMat.style.display = "inline";
+    calc.element.text[0].style.display = "inline";
+    calc.element.text[1].style.display = "none";
+    calc.element.text[2].style.display = "none";
+    calc.element.text[3].style.display = "none";
+    calc.element.operator.style.display = "inline"
+    calc.element.secondryConstant.style.display = "inline";
+    calc.element.primaryConstant.style.display = "inline";
+    calc.element.primaryMat.style.display = "inline";
+    calc.element.secondryMat.style.display = "inline";
 })
--- mark9.js
+++ mark9.js
@@ -1,5 +1,7 @@
 class calculator {
-    gloabalVariable = {
+
+    // element form dom
+    element = {
         primaryRowObject: document.querySelector("#p-row"),
         primaryColObject: document.querySelector("#p-column"),
         secondryRowObject: document.querySelector("#s-row"),
@@ -15,9 +17,295 @@ class calculator {
         inputMat: document.querySelector("#inputMat")
     }
 
-    
+    // this method append matrix position = primary,secondry
+    // choice = fill, zero
+    appendMatrix(row, col, position, choice, mat) {
+
+        if (position == "answer" && choice == "fill") {
+            this.element.answerMatObj.innerHTML = "";
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+
+                    // creating new Matrix for input 
+                    let newMat = document.createElement("input");
+                    newMat.type = 'number';
+                    newMat.value = mat[i][j];
+
+                    // assigning class to new matrix
+                    newMat.classList = 'p-row-col_' + i + "" + j;
+                    this.element.answerMatObj.appendChild(newMat);
+                }
+
+                // adding break line tag
+                let br = document.createElement("br");
+                this.element.answerMatObj.appendChild(br);
+
+            }
+        }
+        if (position == "primary" && choice == "fill-answer") {
+            this.element.primaryMat.innerHTML = "";
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+
+                    // creating new Matrix for input 
+                    let newMat = document.createElement("input");
+                    newMat.type = 'number';
+                    newMat.value = mat[i][j];
+
+                    // assigning class to new matrix
+                    newMat.classList = 'p-row-col_' + i + "" + j;
+                    this.element.primaryMat.appendChild(newMat);
+                }
+
+                // adding break line tag
+                let br = document.createElement("br");
+                this.element.primaryMat.appendChild(br);
+
+            }
+        }
+        if (position == "secondry" && choice == "fill-answer") {
+            this.element.secondryMat.innerHTML = "";
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+
+                    // creating new Matrix for input 
+                    let newMat = document.createElement("input");
+                    newMat.type = 'number';
+                    newMat.value = mat[i][j];
+
+                    // assigning class to new matrix
+                    newMat.classList = 'p-row-col_' + i + "" + j;
+                    this.element.secondryMat.appendChild(newMat);
+                }
+
+                // adding break line tag
+                let br = document.createElement("br");
+                this.element.secondryMat.appendChild(br);
+
+            }
+        }
+        if (position == "primary" && choice == "fill") {
+            let k = 1;
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+
+                    // creating new Matrix for input 
+                    let newMat = document.createElement("input");
+                    newMat.type = 'number';
+                    newMat.value = k;
+
+                    k++;
+
+                    // assigning class to new matrix
+                    newMat.classList = 'p-row-col_' + i + "" + j;
+                    this.element.primaryMat.appendChild(newMat);
+                }
+
+                // adding break line tag
+                let br = document.createElement("br");
+                this.element.primaryMat.appendChild(br);
+
+            }
+        }
+        if (position == "primary" && choice == "zero") {
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+
+                    // creating new Matrix for input 
+                    let newMat = document.createElement("input");
+                    newMat.type = 'number';
+                    newMat.value = 0;
+
+                    // assigning class to new matrix
+                    newMat.classList = 'p-row-col_' + i + "" + j;
+                    this.element.primaryMat.appendChild(newMat);
+                }
+
+                // adding break line tag
+                let br = document.createElement("br");
+                this.element.primaryMat.appendChild(br);
+
+            }
+        }
+        if (position == "secondry" && choice == "fill") {
+            let k = 1;
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+
+                    // creating new Matrix for input 
+                    let newMat = document.createElement("input");
+                    newMat.type = 'number';
+                    newMat.value = k;
+                    k++;
+
+                    // assigning class to new matrix
+                    newMat.classList = 's-row-col_' + i + "" + j;
+                    this.element.secondryMat.appendChild(newMat);
+                }
+
+                // adding break line tag
+                let br = document.createElement("br");
+                this.element.secondryMat.appendChild(br);
+
+            }
+        }
+        if (position == "secondry" && choice == "zero") {
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+
+                    // creating new Matrix for input 
+                    let newMat = document.createElement("input");
+                    newMat.type = 'number';
+                    newMat.value = 0;
+
+                    // assigning class to new matrix
+                    newMat.classList = 's-row-col_' + i + "" + j;
+                    this.element.secondryMat.appendChild(newMat);
+                }
+
+                // adding break line tag
+                let br = document.createElement("br");
+                this.element.secondryMat.appendChild(br);
+
+            }
+        }
+    }
+
+    // this method form matrix choice = fill,blank,zero,primary,secondry
+    getMatrix(row, col, choice) {
+
+        // creating matrix
+        let mat = new Array(row);
+        for (let i = 0; i < row; i++) {
+            mat[i] = new Array(col);
+        }
+
+        // fill the matrix with secondry matrix
+        if (choice == "secondry") {
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+                    //variable matrix feed by user input
+                    mat[i][j] = parseInt(document.querySelector(".s-row-col_" + i + "" + j).value);
+                }
+            }
+        }
+        // fill the matrix with primary matrix
+        if (choice == "primary") {
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+                    //variable matrix feed by user input
+                    mat[i][j] = parseInt(document.querySelector(".p-row-col_" + i + "" + j).value);
+                }
+            }
+        }
+
+        //fill the matrix with zero 
+        if (choice == "zero") {
+            let k = 1;
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+                    mat[i][j] = 0;
+                    k++;
+                }
+            }
+        }
+
+        // fill the matrix with 1,2,3....
+        if (choice == "fill") {
+            let k = 1;
+            for (let i = 0; i < row; i++) {
+                for (let j = 0; j < col; j++) {
+                    mat[i][j] = k;
+                    k++;
+                }
+            }
+        }
+
+        // return empty matrix
+        if (choice == "blank") {
+            return mat;
+        }
+
+        return mat;
+    }
+
+    // this method add two given matrix
+    addition(mat1, mat2, row, col) {
+
+        let c = this.getMatrix(row, col, "blank");
+
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                c[i][j] = mat1[i][j] + mat2[i][j];
+            }
+        }
+
+        return c;
+    }
+    subtraction(mat1, mat2, row, col) {
+
+        let c = this.getMatrix(row, col, "blank");
+
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < col; j++) {
+                c[i][j] = mat1[i][j] - mat2[i][j];
+            }
+        }
+
+        return c;
+    }
+    multiplication(mat1, mat2, pRow, pCol, sCol) {
+
+        let c = this.getMatrix(pRow, pCol, "blank");
+
+        for (let i = 0; i < pRow; i++) {
+            for (let j = 0; j < sCol; j++) {
+                let x = 0;
+                for (let k = 0; k < pCol; k++) {
+                    x += (mat1[i][k] * mat2[k][j]);
+                }
+                c[i][j] = x;
+            }
+        }
+
+        return c;
+    }
+
+
 }
 
-let x = new calculator();
+let calc = new calculator();
+let row = 0;
+let col = 0;
+let sCol = 0;
 
-console.log(x.gloabalVariable)
+
+function enterFun() {
+    calc.appendMatrix(row, col, "primary", "zero");
+    calc.appendMatrix(row, col, "secondry", "zero");
+}
+
+function answerFun() {
+
+   
+
+    let a = calc.getMatrix(row, col, "primary");
+    let b = calc.getMatrix(row, col, "secondry");
+    if (flag == "Addition") {
+        calc.appendMatrix(row, col, "answer", "fill", calc.addition(a, b, row, col));
+        return calc.addition(a,b,row,col);
+    }
+    if (flag == "Subtraction") {
+        calc.appendMatrix(row, col, "answer", "fill", calc.subtraction(a, b, row, col));
+    }
+    if (flag == "Multiplication") {
+        calc.appendMatrix(row, col, "answer", "fill", calc.multiplication(a, b, row, col, sCol));
+    }
+}
+
+function prevAnsewerAtPrimary(){
+    calc.appendMatrix(row,col,"primary","fill-answer",answerFun())
+}
+function prevAnsewerAtSecondry(){
+    calc.appendMatrix(row,col,"secondry","fill-answer",answerFun())
+}
